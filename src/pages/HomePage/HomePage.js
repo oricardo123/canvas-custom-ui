ec2b6d3c14cc7a795da7d6d27e01bd69d2f0d611
Timothy Yee <timothyyee@canas-mbp.lan>
2023-04-01 14:42:05 -0700

    trap background

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,25 +1,10 @@
-import vector from "./Vector/vector";
+import "./Homepage.css";
 const HomePage = () => {
   return (
-    <div className="vector1 grid grid-cols-1">
-      <vector />
+    <div class="container">
+      <div class="left"></div>
+      <div class="right"></div>
     </div>
   );
-
-  /*return (
-    <div className="vector1 grid grid-cols-1">
-      <img
-        src="../images/vector1.svg"
-        className="h-full w-full object-contain row-start-1 col-start-1"
-        alt="vector1"
-      />
-      <img
-        src="../images/vector2.svg"
-        className="vector2 h-full w-full object-contain row-start-1 col-start-1 translate-y-20"
-        alt="vector2"
-      />
-    </div>
-  );*/
 };
-
 export default HomePage;
