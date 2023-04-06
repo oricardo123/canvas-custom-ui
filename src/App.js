750406236b5650345384834ac5f3a828edd3cdd6
Ricardo <ricardoalmeida312@gmail.com>
2023-04-06 22:54:03 +0100

    added react router :)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import Navbar from "./NavBarandFooter/Navbar.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.js";
 import Footer from "./NavBarandFooter/Footer.js";
 import ContactPage from "./pages/ContactPage.js";
 import CatalogPage from "./pages/CatalogPage.js";
 import PortfolioPage from "./pages/PortfolioPage.js";
+import AboutPage from "./pages/AboutPage.js";
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <HomePage/>
-      <Footer/>
-    </div>
-  )
-};
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
--- src/NavBarandFooter/Navbar.js
+++ src/NavBarandFooter/Navbar.js
@@ -1,30 +1,28 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import img from '../images/canvas-logo-brown.jpg';
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import img from '../images/canvas-logo-brown.jpg';
-const navigation = [
-  { name: "Home", href: "#" },
-  { name: "About", href: "#" },
-  { name: "Portfolio", href: "#" },
-  { name: "Catalogue", href: "#" },
-  { name: "Contact", href: "#" },
-];
 
-export default function Navbar() {
+const Navbar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const navigation = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Catalog", href: "/catalog" },
+    { name: "Portfolio", href: "/portfolio" },
+    { name: "Contact", href: "/contact" },
+  ];
 
   return (
-    <div className="bg-white ">
+    <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          className="flex items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
+        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link to="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img className="h-8 w-auto" src={img} alt="canvas custom" />
-            </a>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
@@ -38,29 +36,24 @@ export default function Navbar() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a
+              <Link
                 key={item.name}
-                href={item.href}
+                to={item.href}
                 className="text-sm font-semibold leading-6 text-black-900 hover:text-[#733E1D]"
               >
                 {item.name}
-              </a>
+              </Link>
             ))}
           </div>
         </nav>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-        >
+        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+              <Link to="/" className="-m-1.5 p-1.5">
                 <span className="sr-only"></span>
                 <img className="h-8 w-auto" src={img} alt="" />
-              </a>
+              </Link>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -72,22 +65,25 @@ export default function Navbar() {
             </div>
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </div>
+                <div className="space-y-6">
+                   {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.href}
+                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className="text-sm font-medium text-gray-900">{item.name}</span>
+                </Link>
+              ))}
             </div>
-          </Dialog.Panel>
-        </Dialog>
-      </header>
-    </div>
-  );
-}
+          </div>
+        </div>
+      </Dialog.Panel>
+    </Dialog>
+  </header>
+</div>
+);
+};
+
+export default Navbar;
