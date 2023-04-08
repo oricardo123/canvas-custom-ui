9e2464a7f81fa90823e90a8ab84cb315110c622d
Ricardo <ricardoalmeida312@gmail.com>
2023-04-08 22:37:12 +0100

    improvements to HomeContainer3

--- src/HomePageComponents/HomeContainer3.js
+++ src/HomePageComponents/HomeContainer3.js
@@ -5,9 +5,10 @@ import img3 from "../images/canvas-image-14.jpg";
 const HomeContainer3 = () => {
   return (
     <div className="flex flex-col lg:flex-row justify-center h-auto lg:h-[34.9375rem] w-full HomeContainer3bg ">
-      <div className="flex-row lg:basis-6/12 items-center lg:mr-[3rem] p-8 ">
-        <h1 className="text-white font-bold lg:mt-[13rem]">Why Choose Us?</h1>
-        <p className="text-white mb-[13rem] mt-[1.25rem]">
+      <div className="flex-row h-[10rem] items-center lg:h-auto lg:mr-[8rem] lg:ml-[8rem] p-8 lg:basis-1/2 lg:mt-[12rem]">
+        <h1 className="text-white font-bold text-4xl">Why Choose Us?</h1>
+
+        <p className="text-white mb-[13rem] mt-[1.25rem] text-xl">
           We never lose sight of our clients' requirements and expectations,
           ensuring complete satisfaction. We are committed to promoting designs
           that are:
@@ -18,37 +19,39 @@ const HomeContainer3 = () => {
           <img
             alt="inspecting top of table"
             src={img1}
-            className="rounded-full h-auto w-auto "
+            className="rounded-full h-auto w-auto max-h-[115px] max-w-[115px] basis-1/3"
           />
           <img
             alt="measuring"
             src={img2}
-            className="rounded-full h-auto w-auto "
+            className="rounded-full h-auto w-auto max-h-[115px] max-w-[115px] basis-1/3"
           />
           <img
             alt="stitching onto furniture"
             src={img3}
-            className="rounded-full h-auto w-auto"
+            className="rounded-full h-auto w-auto max-h-[115px] max-w-[115px] basis-1/3"
           />
         </div>
-        <div className="flex flex-col justify-evenly p-2 lg:mr-[11.5rem]">
-          <div >
-            <h1 className="text-white font-bold">Distinctive</h1>
-            <p className="text-white">
+        <div className="flex flex-col justify-evenly p-2 lg:mr-[10.5rem] lg:mt-[3.5rem]">
+          <div className="basis-1/3 lg:mb-4">
+            <h1 className="text-white font-bold text-xl">Distinctive</h1>
+            <p className="text-white text-xl">
               We strive to create pieces that are visually stunning and will add
               a touch of sophistication and elegance.
             </p>
           </div>
-          <div>
-            <h1 className="text-white font-bold">Functional </h1>
-            <p className="text-white">
+          <div className="basis-1/3 lg:mb-4">
+            <h1 className="text-white font-bold text-xl">Functional </h1>
+            <p className="text-white text-xl">
               Our pieces are designed to not only look great, but also serve a
               purpose and make your life easier.
             </p>
           </div>
-          <div>
-            <h1 className="text-white font-bold">Attention to detail</h1>
-            <p className="text-white">
+          <div className="basis-1/3 lg:mb-4">
+            <h1 className="text-white font-bold text-xl">
+              Attention to detail
+            </h1>
+            <p className="text-white text-xl">
               We believe that every element of our furniture should be crafted
               with care and precision.
             </p>
